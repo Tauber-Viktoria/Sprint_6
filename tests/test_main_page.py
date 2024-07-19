64fccc43a1c27eb16548c21a5c9de751a8af3c14
Tauber-Viktoria <viktoriia.tauber@gmail.com>
2024-07-19 17:04:44 +0300

    add to assert

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -9,9 +9,9 @@ class TestMainPage:
         "q_num, expected_result",
         [(i, ANSWERS.get(i)) for i in range(8)]
     )
-    def test_questions(self, main_page, q_num, expected_result):
+    def test_click_to_questions_show_answers(self, main_page, q_num, expected_result):
         main_page.click_to_cookie_button()
         main_page.execute_scroll()
         main_page.click_to_question(q_num)
         result = main_page.get_answer_text(q_num)
-        assert result == expected_result
+        assert result == expected_result, 'Ответ на вопрос не совпадает с ожидаемым значением.'
